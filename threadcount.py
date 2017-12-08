15ba99d9898ca3992a9b3db0afc5cc8cb91fce65
Jack Klika <jhklika@uwm.edu>
2017-12-07 18:12:27 -0600

    IT WORKS sort of. Need to fix exported graphs though

--- threadcount.py
+++ threadcount.py
@@ -1,7 +1,8 @@
 import basc_py4chan
 import nltk
 import inspect
-from collections import Counter
+import time
+from collections import Counter, OrderedDict
 from nltk.corpus import stopwords
 from nltk.tag import pos_tag
 from nltk.tokenize import SpaceTokenizer
@@ -22,9 +23,17 @@ nltk.download('words')
 tokenizer = SpaceTokenizer()
 stop = stopwords.words('english')
 
+# Some running vars
+board = 'g'
+threadcount = 1000 # Amount of threads to get. Set to "1000" or something for all threads
+
+# Set some graph varsk
+title = "/{}/ name frequency {}".format(board, time.time())
+ylabel = "Person Frequency"
+
 
 # 4chan config
-board = basc_py4chan.Board('lit')
+board = basc_py4chan.Board(board)
 
 thread_ids = board.get_all_thread_ids()
 
@@ -36,7 +45,7 @@ name = ""
 namelist = []
 
 for thread_id in thread_ids:
-    if board.thread_exists(thread_id) and n < 2:
+    if board.thread_exists(thread_id) and n < threadcount:
         thread = board.get_thread(thread_id)
         #print("Thread:", thread.url)
         postlist = thread.all_posts
@@ -61,13 +70,22 @@ for thread_id in thread_ids:
                             print(namelist[-1])
         n = n+1
 
-c = sorted(Counter(namelist).items())
-labels, values = zip(*c)
+c = Counter(namelist)
+for cc in c.most_common(200):
+    print("{},{}".format(cc[0],cc[1]))
+cordered = OrderedDict(c.most_common(30))
+labels, values = zip(*cordered.items())
 
 indexes = np.arange(len(labels))
 width = 1
 
-plt.bar(indexes, values, width)
-plt.xticks(indexes + width, labels)
+plt.title(title)
+plt.ylabel(ylabel)
+plt.bar(indexes, values, width, color=["#d6daf0", "#d0d0f0"])
+plt.xticks(indexes, labels, rotation=90)
+
+
+plt.savefig('{}.png'.format(int(time.time())))
 plt.show()
+
 
